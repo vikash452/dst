dbc07c9730d65be2c4155189433261463d7bbff1
Vikash <vikashp0901@gmail.com>
2021-03-01 14:08:16 +0530

    throw garbage page added

--- server.js
+++ server.js
@@ -3,6 +3,17 @@ const bodyParser = require('body-parser');
 const app=express();
 const PORT = process.env.PORT || 5000 ;
 
+// app.use(express.static('public'))
+app.use(bodyParser.json())
+
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname +  '/public/index.html')
+})
+
+app.get('/throwGarbage',(req,res)=>{
+    res.sendFile(__dirname +  '/public/throwGarbage.html')
+})
+
 app.listen(PORT,()=>{
     console.log('server started on port 5000')
 })
