b166509b55956cc9ef3925138fe1756487d40769
Vikash <vikashp0901@gmail.com>
2021-03-08 11:05:18 +0530

    backend working rectified, throw garbage linked to frontend

--- Routes.js
+++ Routes.js
@@ -36,7 +36,7 @@ router.post('/addDustbin',(req,res)=>{
 
 router.put('/throwGarbage',(req,res)=>{
     const areaName=req.body.areaName;
-    const garbageToThrow=req.body.garbageToThrow
+    const garbageToThrow=parseInt(req.body.garbageToThrow)
     Dustbin.findOne({areaName})
     .then((foundDustbin)=>{
         if(!foundDustbin)
@@ -45,7 +45,7 @@ router.put('/throwGarbage',(req,res)=>{
         }
         
         foundDustbin.filled+=garbageToThrow;
-        foundDustbin.status = (foundDustbin.filled)/(foundDustbin.capacity);
+        foundDustbin.status = ((foundDustbin.filled)/(foundDustbin.capacity))*100;
         foundDustbin.save()
         .then((savedDustbin)=>{
             res.status(200).json(savedDustbin)
@@ -63,6 +63,27 @@ router.get('/getDetails',(req,res)=>{
     })
     .catch((err)=>{
         console.log(err)
+    })
+})
+
+router.put('/removeGarbage',(req,res)=>{
+    const areaName=req.body.areaName;
+    Dustbin.findOne({areaName})
+    .then((foundDustbin)=>{
+        if(!foundDustbin)
+        {
+            res.status(400).json({error:'no dustbin with this area name'})
+        }
+        
+        foundDustbin.filled = 0;
+        foundDustbin.status = 0;
+        foundDustbin.save()
+        .then((savedDustbin)=>{
+            res.status(200).json(savedDustbin)
+        })
+        .catch((err)=>{
+            console.log(err)
+        })
     })
 })
 
