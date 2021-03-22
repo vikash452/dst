80657f79eccde843e885d27e0fe68105c10db824
Umang2023 <umang.2023@gmail.com>
2021-03-22 16:29:54 +0530

    Styling updates

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const PORT = process.env.PORT || 5000 ;
 const mongoose=require('mongoose')
 
 require('dotenv').config()
-mongoose.connect(process.env.MONGO_URL,{useNewUrlParser:true,useUnifiedTopology:true,useFindAndModify:false} )
-// mongoose.connect('mongodb://localhost:27017/DST',{useNewUrlParser:true,useUnifiedTopology:true,useFindAndModify:false} );
+// mongoose.connect(process.env.MONGO_URL,{useNewUrlParser:true,useUnifiedTopology:true,useFindAndModify:false} )
+mongoose.connect('mongodb://localhost:27017/DST',{useNewUrlParser:true,useUnifiedTopology:true,useFindAndModify:false} );
 mongoose.connection.on('connected',()=>{
     console.log('connected to database')
 })
